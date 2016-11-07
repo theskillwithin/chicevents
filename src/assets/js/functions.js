3cad6600903ac99fd2bd4d7b66a0543f673d6c1c
theskillwithin <austinppeterson@gmail.com>
2016-11-06 19:40:34 -0700

    .contains and adding swiper dep

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,41 +1,43 @@
-  const chic = {
-    mobileNav () {
-      const mq = window.matchMedia('(max-width: 890px)')
-      const header = document.querySelector('header')
-      const headerNav = header.querySelector('nav')
-      const navUl = headerNav.querySelector('ul')
-      const navHamburger = header.querySelector('.hamburger')
-      const top = document.querySelector('header .top')
+import Swiper from 'swiper'
+const chic = {
+  mobileNav () {
+    const mq = window.matchMedia('(max-width: 890px)')
+    const header = document.querySelector('header')
+    const headerNav = header.querySelector('nav')
+    const navUl = headerNav.querySelector('ul')
+    const navHamburger = header.querySelector('.hamburger')
+    const top = document.querySelector('header .top')
+    if (mq.matches) {
+      headerFire()
+    }
+
+    mq.addListener(() => {
       if (mq.matches) {
         headerFire()
       }
+    })
 
-      mq.addListener(() => {
-        if (mq.matches) {
-          headerFire()
+    function headerFire () {
+      navUl.classList.add('mobile')
+
+      document.addEventListener('click', event => {
+        // console.log(event.currentTarget)
+        if (!event.target.contains(top)) return
+        // if (event.target === top.querySelector('.container')) return
+
+        if (!navUl.classList.contains('mobile')) {
+          navUl.classList.toggle('mobile')
+          // navUser.classList.toggle('mobile')
+          navHamburger.classList.toggle('mobile')
         }
       })
-
-      function headerFire () {
-        navUl.classList.add('mobile')
-
-        document.addEventListener('click', event => {
-          if (event.target === top) return
-          if (event.target === top.querySelector('.container')) return
-
-          if (!navUl.classList.contains('mobile')) {
-            navUl.classList.toggle('mobile')
-            // navUser.classList.toggle('mobile')
-            navHamburger.classList.toggle('mobile')
-          }
-        })
-      }
-
-      navHamburger.addEventListener('click', event => {
-        navUl.classList.toggle('mobile')
-        navHamburger.classList.toggle('mobile')
-        event.stopPropagation()
-      })
     }
+
+    navHamburger.addEventListener('click', event => {
+      navUl.classList.toggle('mobile')
+      navHamburger.classList.toggle('mobile')
+      event.stopPropagation()
+    })
   }
-  chic.mobileNav()
+}
+chic.mobileNav()
